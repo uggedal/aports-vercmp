35fc117ae46a403d4051ef7891819b5c1431132e
Natanael Copa <ncopa@alpinelinux.org>
2014-07-09 13:08:11 +0000

    gnome: refactor the api to be more generic

--- upstream/gnome.lua
+++ upstream/gnome.lua
@@ -2,18 +2,19 @@
 
 http = require("socket.http")
 json = require("cjson")
-ml = require("ml")
 apk = require("apk")
 
-local gnome = {}
-function gnome.find_newer(upkgname, oldver)
-	print("DEBUG: searching:", upkgname)
+local M = {}
+
+local function find_newer(self)
+	local oldver = self.pkg.pkgver
+	print("DEBUG: searching:", self.gnome_name)
 	local baseurl = "http://ftp.gnome.org/pub/GNOME/sources/"
-	local jsonurl = baseurl..upkgname.."/cache.json"
+	local jsonurl = baseurl..self.gnome_name.."/cache.json"
 	local jsondata = assert(http.request(jsonurl))
 	local n,t = unpack(json.decode(jsondata))
 	local latest = oldver or "0"
-	for k,v in pairs(t[upkgname]) do
+	for k,v in pairs(t[self.gnome_name]) do
 		if apk.version_compare(k, latest) == ">" then
 			latest = k
 		end
@@ -24,16 +25,26 @@ function gnome.find_newer(upkgname, oldver)
 	return latest
 end
 
+function M.init(pkg)
+	local provider = {
+		provider_name = "gnome",
+		pkg = pkg,
+	}
 
-function gnome.is_gnome_source(pkg)
 	for source in pkg:remote_sources() do
-		local gnomename = string.match(source, "GNOME/sources/([^/]+)/") or
-			string.match(source, "download.gnome.org/sources/([^/]+)/")
+		local gnomename = string.match(source, "GNOME/sources/([^/]+)/")
+			or string.match(source,
+					"download.gnome.org/sources/([^/]+)/")
 		if gnomename then
-			return gnomename
+			return {
+				gnome_name = gnomename,
+				provider_name = "gnome",
+				find_newer = find_newer,
+				pkg = pkg
+			}
 		end
 	end
 	return nil
 end
 
-return gnome
+return M
