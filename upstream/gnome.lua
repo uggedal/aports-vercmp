7e8e8f211e187764c295ba448dba86f651303d1c
Eivind Uggedal <eivind@uggedal.com>
2015-09-10 10:40:05 +0000

    gnome: simplify version comparison

--- upstream/gnome.lua
+++ upstream/gnome.lua
@@ -5,19 +5,18 @@ apk = require("apk")
 local M = {}
 
 local function find_newer(self)
-	local oldver = self.pkg.pkgver
 	dbg(("%s: gnome: fetching %s"):format(self.pkg.pkgname, self.gnome_name))
 	local baseurl = "http://ftp.gnome.org/pub/GNOME/sources/"
 	local jsonurl = baseurl..self.gnome_name.."/cache.json"
 	local jsondata = assert(http.request(jsonurl))
 	local n,t = unpack(json.decode(jsondata))
-	local latest = oldver or "0"
+	local latest = self.pkg.pkgver or "0"
 	for k,v in pairs(t[self.gnome_name]) do
 		if apk.version_compare(k, latest) == ">" then
 			latest = k
 		end
 	end
-	if latest == oldver then
+	if latest == self.pkg.pkgver then
 		latest = nil
 	end
 	return latest
